910a343713c6e2fa1aa0a7156283651035067ba0
pujita-goraparthi <pujita.goraparthi@ada.ac.uk>
2025-01-30 14:25:39 +0000

    Locations Table added

--- schema.sql
+++ schema.sql
@@ -3,3 +3,12 @@
 -- Enable foreign keys
 PRAGMA foreign_keys = ON;
 
+-- Locations Table
+CREATE TABLE locations (
+    location_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    address TEXT NOT NULL,
+    phone_number TEXT NOT NULL,
+    email TEXT NOT NULL,
+    opening_hours TEXT NOT NULL
+);
