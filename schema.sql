c2fd3a0d3716153bdd21367a36709e85634337c1
pujita-goraparthi <pujita.goraparthi@ada.ac.uk>
2025-01-30 14:32:11 +0000

    Members table added

--- schema.sql
+++ schema.sql
@@ -12,3 +12,16 @@ CREATE TABLE locations (
     email TEXT NOT NULL,
     opening_hours TEXT NOT NULL
 );
+
+-- Members table
+CREATE TABLE members (
+    member_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email UNIQUE TEXT NOT NULL,
+    phone_number TEXT NOT NULL,
+    date_of_birth DATE NOT NULL,
+    join_date DATE NOT NULL,
+    emergency_contact_name TEXT NOT NULL,
+    emergency_contact_phone TEXT NOT NULL
+);
