6ad5306e5008000a494e1992b7b4cce77d2b6a23
pujita-goraparthi <pujita.goraparthi@ada.ac.uk>
2025-02-10 16:27:10 +0000

    equipment_management.sql queries

--- src/equipment_management.sql
+++ src/equipment_management.sql
@@ -1,5 +1,5 @@
 -- Initial SQLite setup
-.open fittrackpro.db
+.open fittrackpro.sqlite
 .mode column
 
 -- Enable foreign key support
@@ -9,8 +9,26 @@
 -- 1. Find equipment due for maintenance
 -- TODO: Write a query to find equipment due for maintenance
 
+SELECT equipment_id,
+    name,
+    next_maintenance_date
+    FROM equipment
+    WHERE next_maintenance_date <= date('now', +'+30 days');
+
 -- 2. Count equipment types in stock
 -- TODO: Write a query to count equipment types in stock
 
+SELECT 
+    type AS equipment_type,
+    COUNT(*) AS count
+FROM equipment
+GROUP BY type;    
+
 -- 3. Calculate average age of equipment by type (in days)
 -- TODO: Write a query to calculate average age of equipment by type (in days)
+
+SELECT 
+    type AS equipment_type,
+    AVG(strftime('%Y', 'now') - strftime('%Y', purchase_date)) AS avg_age_years
+FROM equipment
+GROUP BY type;
